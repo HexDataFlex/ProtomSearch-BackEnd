37c16720b7f7b4f383815667c021ac122eeba87e
Protopixel <protopixel06@gmail.com>
2022-09-05 21:23:11 +0200

    Edited intendation

--- config.php
+++ config.php
@@ -1,18 +1,20 @@
 <?php
-ini_set( "display_errors", true );
-date_default_timezone_set( "Europe/Bratislava" );  // http://www.php.net/manual/en/timezones.php
-define( "DB_DSN", "mysql:host=localhost;dbname=protomsearch" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "x" );
-define( "CLASS_PATH", "classes" );
+ini_set("display_errors", true);
+date_default_timezone_set("Europe/Bratislava");  // http://www.php.net/manual/en/timezones.php
+define("DB_DSN", "mysql:host=localhost;dbname=protomsearch");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "x");
+define("CLASS_PATH", "classes");
+
+define("TOKEN_EXP", 1800);
 
 include_once CLASS_PATH . '/DB.php';
 include_once CLASS_PATH . '/Token.php';
 
 
 function handleException( $exception ) {
-  echo "Sorry, a problem occurred. Please try later.";
-  error_log( $exception->getMessage() );
+  echo json_encode(array("error" => "An unknown error occurred"));
+  error_log($exception->getMessage() . "\n", 3, "/srv/http/protomsearch.log");
 }
 
 set_exception_handler( 'handleException' );
